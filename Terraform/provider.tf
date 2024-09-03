cc628cff47793a7235aedd3d111ceaa9f011ea32
Utkarsh R <utkarsh.rewatkar08@gmail.com>
2024-09-03 12:08:02 +0530

    Added github actions

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,8 +1,14 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.7.0"
+    }
+  
+  }
+}
 
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+provider "azurerm" {
+  features {
+  }
 }
